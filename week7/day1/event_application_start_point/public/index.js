c03b45d799b95297a75dea417a84c705806fa4b7
Harry A. Swan <has.land@hotmail.co.uk>
2016-06-06 15:06:58 +0100

    autosave

---
+++ week7/day1/event_application_start_point/public/index.js
@@ -0,0 +1,38 @@
+
+
+var letsGo = function() {
+
+    var btn = document.getElementById('add-button');
+    var form = document.getElementById('film-form');
+    btn.onclick = addFilm;
+    // form.onsubmit = function() {
+    //     console.log("SUBMIT");
+    //     return addFilm();
+    //     // return false;
+    // };
+    form.onsubmit = addFilm;
+
+
+
+};
+
+var addFilm = function(e) {
+    // e.preventDefault(); <- this or return false at end stops form submitting
+    var txt = document.getElementById('film-text-input');
+    if (txt.value) {
+        appendFilm(txt.value);
+        txt.value = null;
+    }
+    txt.focus();
+    return false;
+};
+
+var appendFilm = function(filmName) {
+    console.log(arguments.callee);
+    var filmList = document.getElementById('film-list');
+    var filmItem = document.createElement('li');
+    filmItem.innerText = filmName;
+    filmList.appendChild(filmItem);
+};
+
+window.onload = letsGo;
